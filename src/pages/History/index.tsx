4a68aed9e66cf7186507eb3a2808d788d24b85aa
Rafael G. Santana <rgs1990419@gmail.com>
2022-07-25 16:00:46 -0300

    Estrutura e estilização página Histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,57 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History() {
-  return <h1>History</h1>
+  return (
+   <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+         {/* O componente HistoryList só foi criado para podermos aplicar um scroll
+         horizontal na tabela, para efeito de resposividade. */}
+         <table>
+            <thead>
+               <tr>
+                  <th>Tarefa</th>
+                  <th>Duração</th>
+                  <th>Início</th>
+                  <th>Status</th>
+               </tr>
+            </thead>
+
+            <tbody>
+               <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há 15 dias</td>
+                  <td>Concluído</td>
+               </tr>
+               <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há 15 dias</td>
+                  <td>Concluído</td>
+               </tr>
+               <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há 15 dias</td>
+                  <td>Concluído</td>
+               </tr>
+               <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há 15 dias</td>
+                  <td>Concluído</td>
+               </tr>
+               <tr>
+                  <td>Tarefa</td>
+                  <td>20 minutos</td>
+                  <td>Há 15 dias</td>
+                  <td>Concluído</td>
+               </tr>
+            </tbody>
+         </table>
+      </HistoryList>
+   </HistoryContainer>
+  )
 }
