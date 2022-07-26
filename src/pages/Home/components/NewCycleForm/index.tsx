781a5ce6380bb4e48bd4ac0b17bccc2d3299b5e1
Rafael G. Santana <rgs1990419@gmail.com>
2022-07-26 19:45:43 -0300

    Exemplo de Prop Drilling no React

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,10 +1,41 @@
+import { useForm } from "react-hook-form";
+// '@hookform/resolvers/zod, vai permitir a integração do React-Hook-Form com a
+// biblioteca de validação 'zod'
+import { zodResolver } from '@hookform/resolvers/zod';
+// Importa o zod desta forma, pois a lib zod não possui um export default, e para
+// não precisarmos importar função por função entre chaves, utilizamos a sintaxe
+// * as zod, assim importamos todas as funções de uma só vez.
+import * as zod from 'zod';
+
 import {
    FormContainer,
    TaskInput,
    MinutesAmountInput,
 } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+   task: zod.string().min(1, 'Informe a tarefa.'),
+   minutesAmount: zod
+      .number()
+      .min(1, 'O ciclo deve ser de no mínimo 5 minutos.')
+      .max(60, 'O ciclo deve ser de no máximo 60 minutos.')
+});
+
 export function NewCycleForm{
+   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+         task: '',
+         minutesAmount: 0
+      }
+   });
+
+   // interface NewCycleFormData {
+   //    task: string;
+   //    minutesAmount: number;
+   // }
+   type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+   
    return(
       <FormContainer>
          <label htmlFor="task">Vou trabalhar em</label>
