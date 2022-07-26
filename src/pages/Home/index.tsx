594e211a01bd542689200fed246c9083a707e001
Rafael G. Santana <rgs1990419@gmail.com>
2022-07-26 18:48:38 -0300

    Countdown - funcionalidade de concluir o ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,7 +25,7 @@ const newCycleFormValidationSchema = zod.object({
    task: zod.string().min(1, 'Informe a tarefa.'),
    minutesAmount: zod
       .number()
-      .min(5, 'O ciclo deve ser de no mínimo 5 minutos.')
+      .min(1, 'O ciclo deve ser de no mínimo 5 minutos.')
       .max(60, 'O ciclo deve ser de no máximo 60 minutos.')
 });
 
@@ -41,6 +41,7 @@ interface Cycle {
    minutesAmount: number;
    startDate: Date;
    interruptDate?: Date;
+   finishedDate?: Date;
 }
 
 export function Home() {
@@ -59,18 +60,39 @@ export function Home() {
    // Buscar o ciclo ativo, no estado cycles, com base na informação contida no estado activeCycleId
    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
 
+   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+   
    useEffect(() => {
       let interval: number;
+      
       if (activeCycle) {
          interval = setInterval(() => {
-            setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+            const secondsDifference = differenceInSeconds(
+               new Date(), activeCycle.startDate
+            );
+
+            if (secondsDifference >= totalSeconds) {
+               setCycles(state =>
+                  state.map((cycle) => {
+                     if(cycle.id === activeCycleId) {
+                        return {...cycle, finishedDate: new Date()}
+                     } else {
+                        return cycle
+                     }
+                  })
+               );
+               setAmountSecondsPassed(totalSeconds);
+               clearInterval(interval);
+            } else {
+               setAmountSecondsPassed(secondsDifference);
+            }
          }, 1000)
       }
 
       return () => {
          clearInterval(interval);
       }
-   }, [activeCycle]);
+   }, [activeCycle, totalSeconds]);
 
    function handleCreateNewCycle(data: NewCycleFormData) {
       const id = String(new Date().getTime());
@@ -93,19 +115,24 @@ export function Home() {
    }
 
    function handleInterruptCycle() {
-      setCycles(cycles.map((cycle) => {
-         if(cycle.id === activeCycleId) {
-            return {...cycle, interruptDate: new Date()};
-         } else {
-            return cycle;
-         }
-      }));
+      setCycles(
+         state => state.map((cycle) => {
+            if(cycle.id === activeCycleId) {
+               return {...cycle, interruptDate: new Date()};
+            } else {
+               return cycle;
+            }
+         })
+      );
 
       setActiveCycleId(null);
+      
    }
    console.log(cycles)
+   console.log(activeCycle)
+   console.log(activeCycleId)
 
-   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+   
    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
    const minutesAmountLeft = Math.floor(currentSeconds / 60);
@@ -161,7 +188,7 @@ export function Home() {
                   // de acordo com o valor passado como valor de step. No ex. abaixo
                   // 5, 10, 15
                   step={5}
-                  min={5} // valor mínimo
+                  min={1} // valor mínimo
                   max={60} // valor máximo
                   {...register('minutesAmount', { valueAsNumber: true })}
                   disabled={!!activeCycle}
