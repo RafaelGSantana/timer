21a7a1fd7d2877c6148d28f5c52ef2b2ee5c8892
Rafael G. Santana <rgs1990419@gmail.com>
2022-07-27 17:36:21 -0300

    Listando informações na tela de histórico de ciclos

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -27,46 +27,26 @@ export function History() {
                </thead>
 
                <tbody>
-                  <tr>
-                     <td>Tarefa</td>
-                     <td>20 minutos</td>
-                     <td>Há 15 dias</td>
-                     <td>
-                        <Status statusColor="yellow">Em andamento</Status>
-                     </td>
-                  </tr>
-                  <tr>
-                     <td>Tarefa</td>
-                     <td>20 minutos</td>
-                     <td>Há 15 dias</td>
-                     <td>
-                        <Status statusColor="green">Concluído</Status>
-                     </td>
-                  </tr>
-                  <tr>
-                     <td>Tarefa</td>
-                     <td>20 minutos</td>
-                     <td>Há 15 dias</td>
-                     <td>
-                        <Status statusColor="red">Interrompido</Status>
-                     </td>
-                  </tr>
-                  <tr>
-                     <td>Tarefa</td>
-                     <td>20 minutos</td>
-                     <td>Há 15 dias</td>
-                     <td>
-                        <Status statusColor="green">Concluído</Status>
-                     </td>
-                  </tr>
-                  <tr>
-                     <td>Tarefa</td>
-                     <td>20 minutos</td>
-                     <td>Há 15 dias</td>
-                     <td>
-                        <Status statusColor="green">Concluído</Status>
-                     </td>
-                  </tr>
+                  {cycles.map(cycle => {
+                     return (
+                        <tr key={cycle.id}>
+                           <td>{cycle.task}</td>
+                           <td>{cycle.minutesAmount} minutos</td>
+                           <td>{cycle.startDate.toISOString()}</td>
+                           <td>
+                              {cycle.finishedDate && (
+                                 <Status statusColor="green">Concluído</Status>
+                              )}
+                              {cycle.interruptDate && (
+                                 <Status statusColor="red">Interrompido</Status>
+                              )}
+                              {(!cycle.finishedDate && !cycle.interruptDate) && (
+                                 <Status statusColor="yellow">Em andamento</Status>
+                              )}
+                           </td>
+                        </tr>
+                     )
+                  })}
                </tbody>
             </table>
          </HistoryList>
