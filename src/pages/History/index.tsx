0c632efbbad3f252f11d589443ee0c32195219e5
Rafael G. Santana <rgs1990419@gmail.com>
2022-07-27 17:40:46 -0300

    Formatação da data de início do ciclo com date-fns

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
+
 
 export function History() {
    const { cycles } = useContext(CyclesContext);
@@ -32,7 +35,10 @@ export function History() {
                         <tr key={cycle.id}>
                            <td>{cycle.task}</td>
                            <td>{cycle.minutesAmount} minutos</td>
-                           <td>{cycle.startDate.toISOString()}</td>
+                           <td>{formatDistanceToNow(cycle.startDate, {
+                              addSuffix: true,
+                              locale: ptBR
+                           })}</td>
                            <td>
                               {cycle.finishedDate && (
                                  <Status statusColor="green">Concluído</Status>
