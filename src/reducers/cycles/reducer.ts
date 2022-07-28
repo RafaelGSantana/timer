7850332714e8eab85ca07ecd13080ebfd1a462c1
Rafael G. Santana <rgs1990419@gmail.com>
2022-07-28 13:15:29 -0300

    Reducers - Utilizando a biblioteca Immer no reducer

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from 'immer';
+
 import { ActionTypes } from "./actions";
 
 export interface Cycle {
@@ -17,35 +19,38 @@ interface CyclesState {
 export function cyclesReducer(state: CyclesState, action: any) {
    switch(action.type) {
       case ActionTypes.ADD_NEW_CYCLE:
-         return {
-            ...state,
-            cycles: [...state.cycles, action.payload.newCycle],
-            activeCycleId: action.payload.newCycle.id
-         }
+         return produce(state, draft => {
+            draft.cycles.push(action.payload.newCycle);
+            draft.activeCycleId = action.payload.newCycle.id;
+         })
       case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-         return {
-            ...state,
-            cycles: state.cycles.map(cycle => {
-               if (cycle.id === state.activeCycleId) {
-                  return { ...cycle, interruptDate: new Date() }
-               } else {
-                  return cycle
-               }
-            }),
-            activeCycleId: null
+         const currentCycleIndexForInterruptCycleAction = state.cycles.findIndex(cycle => {
+            return cycle.id === state.activeCycleId;
+         });
+
+         // Se o findIndex não encontrar o index correspondente à verificação de igualdade de id acima, retornará -1
+         if(currentCycleIndexForInterruptCycleAction < 0) {
+            return state;
          }
+
+         return produce(state, draft => {
+            draft.activeCycleId = null;
+            draft.cycles[currentCycleIndexForInterruptCycleAction].interruptDate = new Date();
+         })
       case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-         return {
-            ...state,
-            cycles: state.cycles.map(cycle => {
-               if (cycle.id === state.activeCycleId) {
-                  return { ...cycle, finishedDate: new Date() }
-               } else {
-                  return cycle
-               }
-            }),
-            activeCycleId: null
+         const currentCycleIndexForFinishedCycleAction = state.cycles.findIndex(cycle => {
+            return cycle.id === state.activeCycleId;
+         });
+
+         // Se o findIndex não encontrar o index correspondente à verificação de igualdade de id acima, retornará -1
+         if(currentCycleIndexForFinishedCycleAction < 0) {
+            return state;
          }
+
+         return produce(state, draft => {
+            draft.cycles[currentCycleIndexForFinishedCycleAction].finishedDate = new Date();
+            draft.activeCycleId = null;
+         })
       default:
          return state;
    }
